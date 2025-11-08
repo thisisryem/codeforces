7dbae3f6413101f2c18aa9c881df21a019d59ae3
Jabir Ahmad Ryem <167877253+thisisryem@users.noreply.github.com>
2025-11-08 10:25:43 +0600

    112A - Petya and Strings

---
+++ 112A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+    int n= a.size();
+    
+    for(int i=0; i<n; i++)
+    {
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+    }
+    if(a<b)
+    {
+        cout << -1 << endl;
+    }
+    else if(a>b)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+}
